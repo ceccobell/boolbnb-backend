6a9c85a3251bc25d62d922865aadd5828b659f27
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-10-30 15:31:15 +0100

    [UPD] created migration for apartments and services

---
+++ database/migrations/2024_10_30_142638_create_apartments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('property', 30);
+            $table->string('city', 15);
+            $table->string('address');
+            $table->string('description', 50);
+            $table->integer('n_rooms');
+            $table->integer('n_beds');
+            $table->integer('n_bathrooms');
+            $table->string('square_meters', 50);
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->string('status', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
