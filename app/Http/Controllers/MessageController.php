56464962f29a73d3438245627d2fd04a1629875e
Rivio98 <rivaldogjoni1998@gmail.com>
2024-11-12 22:27:03 +0100

    [UPD]Fix issue for message controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -11,16 +11,20 @@ class MessageController extends Controller
     {
         $validatedData = $request->validate([
             'apartment_id' => 'required|exists:apartments,id',
-            'sender_name' => 'nullable|string|max:255',
+            'sender_name' => 'required|string|max:255',
+            'sender_surname' => 'required|string|max:255',
             'sender_email' => 'required|email|max:255',
             'sender_message_text' => 'required|string',
+            'sender_message_object' => 'required|string|max:255|',
         ]);
 
         $message = Message::create([
             'apartment_id' => $validatedData['apartment_id'],
-            'sender_name' => $validatedData['sender_name'] ?? null,
+            'sender_name' => $validatedData['sender_name'],
+            'sender_surname' => $validatedData['sender_surname'],
             'sender_email' => $validatedData['sender_email'],
             'sender_message_text' => $validatedData['sender_message_text'],
+            'sender_message_object' => $validatedData['sender_message_object'],
         ]);
 
         return response()->json(['message' => 'Message sent successfully!'], 201);
